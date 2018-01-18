9624fb5a840fa68af083d9ee84859ac0afa54450
Angelo Russoniello and Tim Firman <angelo@>
2018-01-18 15:39:36 -0800

    adds loop through each word

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,35 @@ function isVowel(c) {
     return ['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) !== -1
 }
 
-var phrase = function (englishPhrase) {
-  if (englishPhrase === '') {
+var eWord = function (englishWord) {
+  if (englishWord === '') {
     alert ("Please enter a phrase to be translated");
-  } else if (isNaN(englishPhrase)){
-      if (isVowel(englishPhrase.charAt(0))) {
-        alert (englishPhrase + 'way');
+  } else if (isNaN(englishWord)){
+      if (isVowel(englishWord.charAt(0))) {
+        return englishWord + 'way';
       } else {
         for (var nconsonants = 1; nconsonants <=6; nconsonants +=1) {
-          if (isVowel(englishPhrase.charAt(nconsonants))) {
+          if (isVowel(englishWord.charAt(nconsonants))) {
             break;
           }
         }
-        alert (englishPhrase.slice(nconsonants) + englishPhrase.slice(0,nconsonants) + 'ay');
+        return englishWord.slice(nconsonants) + englishWord.slice(0,nconsonants) + 'ay';
       };
   } else {
     alert ("A number cannot be used");
   }
 };
+
+var phrase = function (englishPhrase) {
+  var finalPhrase = ''
+  var arrayOfWords = englishPhrase.split(' ');
+    for (var index = 0; index <arrayOfWords.length; index +=1){
+
+    finalPhrase = finalPhrase + eWord(arrayOfWords[index]) + ' ';
+  }
+  alert (finalPhrase);
+};
+
 // user interface logic
 $(document).ready(function() {
   $("form#pigLatin").submit(function(event) {
