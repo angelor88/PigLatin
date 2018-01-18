cf443f0b9c3a08f7ee26c708ac8efa29d585ad3b
Angelo Russoniello and Tim Firman <angelo@>
2018-01-18 14:05:01 -0800

    adds loop for determining number of initial consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 function isVowel(c) {
     return ['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) !== -1
 }
+
 var phrase = function (englishPhrase) {
   if (englishPhrase === '') {
     alert ("Please enter a phrase to be translated");
   } else if (isNaN(englishPhrase)){
-     if (isVowel(englishPhrase.charAt(0))) {
-       alert ('Yes, it is a vowel!')
-     };
+      if (isVowel(englishPhrase.charAt(0))) {
+        alert (englishPhrase + 'way');
+      } else {
+        for (var nconsonants = 1; nconsonants <=6; nconsonants +=1) {
+          if (isVowel(englishPhrase.charAt(nconsonants))) {
+            break;
+          }
+        }
+        alert (englishPhrase.slice(nconsonants) + englishPhrase.slice(0,nconsonants) + 'ay');
+      };
   } else {
     alert ("A number cannot be used");
   }
@@ -18,8 +26,6 @@ $(document).ready(function() {
     event.preventDefault();
     var inputPhrase = $("input#startPhrase").val();
     var result = phrase(inputPhrase);
-
-    $(".result").text(year);
 
     $("#result").show();
   });
